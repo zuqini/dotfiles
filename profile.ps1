0e6758b0a34a4201fabae7794ad4b43ff1465f8f
zuqini <lizuqiliang@gmail.com>
2024-01-04 12:48:20 -0800

    Add language servers to path for helix

--- profile.ps1
+++ profile.ps1
@@ -1,3 +1,10 @@
 Set-Alias vim nvim
-Set-Alias vid C:\Users\lizuq\neovide.exe
+Set-Alias vid $HOME\neovide.exe
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
+
+$env:PATH = [string]::join(
+	';',
+	$env:PATH,
+	$HOME + '\AppData\Local\nvim-data\mason\packages\lua-language-server\extension\server\bin',
+	$HOME + '\AppData\Local\nvim-data\mason\packages\omnisharp'
+)
