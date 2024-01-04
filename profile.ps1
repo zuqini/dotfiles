d18672cd8997f5afeb99b8c25f22f9631bc15ea0
zuqini <lizuqiliang@gmail.com>
2024-01-04 12:48:21 -0800

    add command for godot vim integration

--- profile.ps1
+++ profile.ps1
@@ -1,3 +1,4 @@
+Function vimgd {nvim --listen 127.0.0.1:9696}
 Set-Alias vim nvim
 Set-Alias vid $HOME\neovide.exe
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
