6506ff8635ce8a995940a7ee615da03c50814aea
zuqini <lizuqiliang@gmail.com>
2024-01-04 12:48:19 -0800

    kitty config

--- install.sh
+++ install.sh
@@ -2,3 +2,6 @@ ln -s ~/dotfiles/nvim ~/.config
 ln -s ~/dotfiles/.themes ~
 ln -s ~/dotfiles/.tmux.conf ~
 ln -s ~/dotfiles/.ideavimrc ~
+
+mkdir ~/.config/kitty
+ln -s ~/dotfiles/kitty.conf ~/.config/kitty/
