d7e35b88566af6c6efd2eb5bd4211ae3b45c6a3f
zuqini <lizuqiliang@gmail.com>
2024-01-04 12:48:21 -0800

    scrollback lines

--- .wezterm.lua
+++ .wezterm.lua
@@ -8,13 +8,17 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
+-----------------------------config-----------------------------------
+
 if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
   config.default_prog = { "C:/Program Files/PowerShell/7/pwsh.exe" }
 end
 
+config.scrollback_lines = 50000
 
-config.color_scheme = 'GruvboxDarkHard'
 config.window_background_opacity  = 0.95
+config.color_scheme = 'GruvboxDarkHard'
+
 config.font = wezterm.font('FiraCode Nerd Font')
 config.font_size = 16
 
