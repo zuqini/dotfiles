3a2da3d346f5045f374ea98b527736bd910f084c
zuqini <lizuqiliang@gmail.com>
2024-01-04 12:48:19 -0800

    install instructions

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # .dotfiles
 Work in progress
 
+```
+$ git clone --recurse-submodules --remote-submodules https://github.com/zuqini/dotfiles.git ~/dotfiles
+$ ~/dotfiles/install.sh
+```
--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-ln -s ~/.dotfiles/.config/nvim ~/.config
-ln -s ~/.dotfiles/.themes ~
-ln -s ~/.dotfiles/.tmux.conf ~
+ln -s ~/dotfiles/.config/nvim ~/.config
+ln -s ~/dotfiles/.themes ~
+ln -s ~/dotfiles/.tmux.conf ~
