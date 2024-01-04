20019e71408e68104fd24dd88e8d2aff8dce50f5
zuqini <lizuqiliang@gmail.com>
2024-01-04 12:48:21 -0800

    add llvm to path and automatically initialive zsh plugins

--- install.sh
+++ install.sh
@@ -4,3 +4,7 @@ ln -s ~/dotfiles/kitty ~/.config
 ln -s ~/dotfiles/.tmux.conf ~
 ln -s ~/dotfiles/.ideavimrc ~
 
+git clone https://github.com/jeffreytse/zsh-vi-mode \
+  ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-vi-mode
+git clone https://github.com/zsh-users/zsh-autosuggestions \
+  ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
