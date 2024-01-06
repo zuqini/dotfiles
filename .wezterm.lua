54c1acf82f93c614f409f5d957f68368f3951fb7
zuqini <lizuqiliang@gmail.com>
2024-01-06 15:40:19 -0800

    back to nushell

--- .wezterm.lua
+++ .wezterm.lua
@@ -33,9 +33,9 @@ config.keys = {
 }
 
 if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
-  config.window_background_opacity  = 0.6
+  config.window_background_opacity  = 0.8
   config.win32_system_backdrop = 'Acrylic'
-  config.default_prog = { 'pwsh.exe' }
+  config.default_prog = { 'C:/Program Files/nu/bin/nu.exe' }
   config.launch_menu = {}
   table.insert(config.launch_menu, {
     label = 'PowerShell',
