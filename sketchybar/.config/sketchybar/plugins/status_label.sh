e8bbe6b744ff916147e796862f020e6ed9cbcc2f
zuqini <lizuqiliang@gmail.com>
2024-11-21 16:17:39 -0800

    do not draw if no notifications

--- sketchybar/.config/sketchybar/plugins/status_label.sh
+++ sketchybar/.config/sketchybar/plugins/status_label.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
 STATUS_LABEL="$(lsappinfo -all info -only StatusLabel $1 | sed -nr 's/\"StatusLabel\"=\{ \"label\"=\"(.+)\" \}$/\1/p')"
-sketchybar --set "$NAME" label="$STATUS_LABEL"
+if [ -z "${STATUS_LABEL}" ]; then
+    sketchybar --set "$NAME" label="$STATUS_LABEL" drawing=off
+else
+    sketchybar --set "$NAME" label="$STATUS_LABEL" drawing=on
+fi
