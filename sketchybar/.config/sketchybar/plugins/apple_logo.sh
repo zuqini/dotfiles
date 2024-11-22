ba4f7e50a31be5bdbcbf67ab5aa4679c553d5550
zuqini <lizuqiliang@gmail.com>
2024-11-22 11:02:42 -0800

    hover effects

--- sketchybar/.config/sketchybar/plugins/apple_logo.sh
+++ sketchybar/.config/sketchybar/plugins/apple_logo.sh
@@ -2,8 +2,10 @@
 
 if [ "$SENDER" = "mouse.exited.global" ]; then
   sketchybar -m --set $NAME popup.drawing=off
+  sketchybar -m --set $NAME background.drawing=off
 fi
 
 if [ "$SENDER" = "mouse.entered" ]; then
   sketchybar -m --set $NAME popup.drawing=on
+  sketchybar -m --set $NAME background.drawing=on
 fi
