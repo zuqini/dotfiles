61d534a8b9779f0b228ac5631ee5a52f04f17d47
zuqini <lizuqiliang@gmail.com>
2024-11-21 23:37:02 -0800

    polish wifi behaviors

--- sketchybar/.config/sketchybar/plugins/wifi.sh
+++ sketchybar/.config/sketchybar/plugins/wifi.sh
@@ -3,8 +3,10 @@
 # The wifi_change event supplies a $INFO variable in which the current SSID
 # is passed to the script.
 INFO="$(networksetup -listallhardwareports | awk '/Wi-Fi/{getline; print $2}' | xargs networksetup -getairportnetwork | sed "s/Current Wi-Fi Network: //")"
-
 if [ "$SENDER" = "wifi_change" ]; then
-  WIFI=${INFO:-"Not Connected"}
-  sketchybar --set $NAME label="${WIFI}"
+  if [[ $INFO = *"You are not associated with an AirPort network."* ]]; then
+      sketchybar --set $NAME label="Not Connected"
+  else
+      sketchybar --set $NAME label="${INFO}"
+  fi
 fi
