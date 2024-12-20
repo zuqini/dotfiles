0514045a9c4a081604708d6be3de6e6bede18da6
zuqini <lizuqiliang@gmail.com>
2024-12-20 10:46:53 -0800

    update color to match statusline

--- sketchybar/.config/sketchybar/plugins/aerospace.sh
+++ sketchybar/.config/sketchybar/plugins/aerospace.sh
@@ -4,7 +4,7 @@
 # chmod +x ~/.config/sketchybar/plugins/aerospace.sh
 
 if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-    sketchybar --set $NAME label.color=0xff8effaf
+    sketchybar --set $NAME label.color=0xffaaeeec
 else
     sketchybar --set $NAME label.color=0xff587b7b
 fi
