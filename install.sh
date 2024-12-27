7e07feaa201d7bdb3bf6005218c4a534ca70e30c
zuqini <lizuqiliang@gmail.com>
2024-12-27 00:16:02 -0800

    install ghostty in install script

--- install.sh
+++ install.sh
@@ -7,6 +7,14 @@ else
   echo "Homebrew already installed"
 fi
 
+which -s ghostty
+if [[ $? != 0 ]] ; then
+  echo "Installing Ghostty"
+  brew install ghostty
+else
+  echo "Ghostty already installed"
+fi
+
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
