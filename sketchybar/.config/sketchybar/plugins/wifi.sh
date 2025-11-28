178fd71bb3b5695d5d25a84058ecdd13f28bcce1
zuqini <lizuqiliang@gmail.com>
2025-11-27 20:00:15 -0800

    more reliable wifi detection in sketchybar

--- sketchybar/.config/sketchybar/plugins/wifi.sh
+++ sketchybar/.config/sketchybar/plugins/wifi.sh
@@ -3,10 +3,14 @@
 # The wifi_change event supplies a $INFO variable in which the current SSID
 # is passed to the script.
 if [ "$SENDER" = "wifi_change" ]; then
-  INFO="$(networksetup -listallhardwareports | awk '/Wi-Fi/{getline; print $2}' | xargs networksetup -getairportnetwork | sed "s/Current Wi-Fi Network: //")"
-  if [[ $INFO = *"You are not associated with an AirPort network."* ]]; then
+  IP_SUMMARY_INFO="$(ipconfig getsummary en0 | awk -F ' SSID : '  '/ SSID : / {print $2}')"
+  if [[ $IP_SUMMARY_INFO = "" ]]; then
       sketchybar --set $NAME label="Not Connected"
+  elif [[ $IP_SUMMARY_INFO = *"<redacted>"* ]]; then
+      # special case for when IP_SUMMARY_INFO is redacted
+      NETWORK_SETUP_INFO="$(networksetup -listpreferredwirelessnetworks en0 | sed -n '2p' | sed 's/^[[:space:]]*//;s/[[:space:]]*$//')"
+      sketchybar --set $NAME label="$NETWORK_SETUP_INFO"
   else
-      sketchybar --set $NAME label="${INFO}"
+      sketchybar --set $NAME label="$IP_SUMMARY_INFO"
   fi
 fi
